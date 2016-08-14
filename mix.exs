b40bde2432f73585c3e92b46bf63fe1e94f07bfa
Sander Hahn <sanderhahn@gmail.com>
2016-08-14 12:38:57 +0200

    add hex metadata

--- mix.exs
+++ mix.exs
@@ -2,12 +2,16 @@ defmodule Markaby.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :markaby,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :markaby,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+    ]
   end
 
   def application do
@@ -20,4 +24,20 @@ defmodule Markaby.Mixfile do
       {:mix_test_watch, "~> 0.2", only: [:dev, :test]},
     ]
   end
+
+  defp description do
+    """
+    Markaby clone in Elixir.
+    """
+  end
+
+  defp package do
+    [
+      name: :markaby,
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Sander Hahn"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/sanderhahn/markaby"},
+    ]
+  end  
 end
